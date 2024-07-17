99dab136d6cbac9349611e5cf1752c2237652aa9
CharaD7 <jijakahn6@gmail.com>
2024-07-17 20:28:00 +0000

    Removed option to launch designer in dark mode: Doesn't work

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ from Custom_Widgets.QAppSettings import QAppSettings
 ########################################################################
 ## MAIN WINDOW CLASS
 ########################################################################
-sys.argv += ['-platform', 'window:darkmode=2']
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self)
